d70b81232136de052bd85f2ccf6e851f91e47be6
chett705 <chetranicky@gmail.com>
2026-04-29 11:56:09 +0700

    change color form login

--- resources/views/Backend/Auth/login.blade.php
+++ resources/views/Backend/Auth/login.blade.php
@@ -14,14 +14,14 @@
                     <label class="block text-sm font-medium text-gray-700 mb-1">Email Address / Username</label>
                     <input type="text"
                         name="username"
-                        class="w-full px-4 py-3 border border-gray-200 rounded-lg focus:ring-2 focus:ring-[#003366] focus:border-transparent outline-none transition-all"
+                        class="w-full px-4 py-3 border border-gray-200 rounded-lg focus:ring-1 focus:ring-[#ED1C24] focus:border-transparent outline-none transition-all"
                         placeholder="Enter email or username"
                         required>
                 </div>
 
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                    <input type="password" name="password" class="w-full px-4 py-2 border border-gray-200 rounded-lg focus:ring-2 focus:ring-[#003366] focus:border-transparent outline-none transition-all">
+                    <input type="password" name="password" class="w-full px-4 py-2 border border-gray-200 rounded-lg focus:ring-1 focus:ring-[#ED1C24] focus:border-transparent outline-none transition-all">
                 </div>
             </div>
 
